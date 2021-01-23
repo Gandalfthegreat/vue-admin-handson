4e8f5f72efe353709ad58062c80aead6ab990e35
wangchao11 <wangchao11@kuaishou.com>
2021-01-23 21:23:28 +0800

    feat: add basic style

--- src/views/helloWorld/index.vue
+++ src/views/helloWorld/index.vue
@@ -1,63 +1,32 @@
 <template>
-  <div class="container">
-    <h1>健康小助手</h1>
-    <el-form :model="form" label="表单">
-      <my-input label="breakfest" :form="form"></my-input>
-      <my-input label="lunch" :form="form"></my-input>
-      <my-input label="dinner" :form="form"></my-input>
-      <el-form-item>
-        <el-button type="primary" size="mini" @click="submitForm">提交表单</el-button>
-      </el-form-item>
-    </el-form>
+  <div>
+    <h1>HelloWorld</h1>
+    <input v-model="msg" />
+    {{ msg }}
+    <button @click="showMsg">button</button>
+    <router-view />
   </div>
 </template>
- 
+
 <script>
-import MyInput from "./component/MyInput";
 export default {
-  components: {
-    MyInput,
-  },
   data() {
     return {
-      form: {
-        breakfest: "",
-        lunch: "",
-        dinner: "",
-      },
+      msg: ""
     };
   },
   methods: {
-    submitForm() {
-      alert(JSON.stringify(this.form));
-    },
-  },
+    showMsg() {
+      alert(this.msg);
+    }
+  }
 };
 </script>
 
 <style scoped>
-.container .box {
+.box {
   width: 200px;
   height: 200px;
 }
-
-.container /deep/ .el-button {
-  width: 400px;
-}
-.container /deep/.el-input {
-  width: auto;
-}
-.el-form .el-input {
-  width: 50%;
-  min-width: 200px;
-}
-.container {
-  margin-left: 50px;
-}
-.container .el-button {
-  width: 100px;
-}
-</style> 
-
-
+</style>
 
--- src/views/todos/index.vue
+++ src/views/todos/index.vue
@@ -1,54 +1,34 @@
 <template>
-  <div>
-    <section class="todoapp">
-      <header class="header">
-        <h1>todos</h1>
-        <input
-          class="new-todo"
-          placeholder="What is the next?"
-          v-model="newTodo"
-          @keyup.enter="addTodo"
-        />
-      </header>
-      <section class="main" v-show="todos.length">
-        <ul class="todo-list">
-          <li
-            v-for="todo in todos"
-            class="todo"
-            :key="todo.id"
-            :class="{completed: todo.completed, editing: todo.id === editedTodo.id}"
-          >
-            <div class="view">
-              <input class="toggle" type="checkbox" v-model="todo.completed" />
-              <label @dblclick="editTodo(todo)">{{todo.title}}</label>
-              <button class="destory" @click="removeTodo(todo)"></button>
-            </div>
-
-            <input
-              class="edit"
-              type="text"
-              v-model="editedTodo.title"
-              @blur="doneEdit(editedTodo)"
-              @keyup.enter="doneEdit(editedTodo)"
-              @keyup.esc="cancelEdit()"
-            />
-          </li>
-        </ul>
-      </section>
-
-      <footer class="footer" v-show="todos.length">
-        <span class="todo-count">
-          <strong>{{remaining}}</strong>
-          {{ remaining | pluralize}} left
-        </span>
-        <button
-          class="clear-completed"
-          @click="removeCompleted"
-          v-show="todos.length > remaining"
-        >Clear Completed</button>
-      </footer>
+  <section class="todoapp">
+    <header class="header">
+      <h1>todos</h1>
+      <input class="new-todo" v-model="newTodo" @keyup.enter="addTodo" />
+    </header>
+    <section class="main" v-show="todos.length">
+      <ul class="todo-list">
+        <li
+          class="todo"
+          v-for="todo in todos"
+          :key="todo.id"
+          :class="{completed: todo.completed, editing: todo.id === editedTodo.id}"
+        >
+          <div class="view">
+            <input class="toggle" type="checkbox" v-model="todo.completed" />
+            <label @dblclick="editTodo(todo)">{{ todo.title}}</label>
+            <button class="destroy" @click="removeTodo(todo)"></button>
+          </div>
+          <input
+            class="edit"
+            type="text"
+            v-model="editedTodo.title"
+            @blur="doneEdit(editedTodo)"
+            @keyup.enter="doneEdit(editedTodo)"
+            @keyup.esc="cancelEdit()"
+          />
+        </li>
+      </ul>
     </section>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -61,46 +41,31 @@ export default {
       editedTodo: {}
     };
   },
-  computed: {
-    remaining() {
-      return this.todos.filter(x => !x.completed).length;
-    }
-  },
-  filters: {
-    pluralize(num) {
-      return num > 1 ? "items" : "item";
-    }
-  },
   methods: {
-    editTodo(todo) {
-      this.editTodo = { ...todo };
+    addTodo() {
+      if (!this.newTodo) return;
+      this.todos.unshift({
+        title: this.newTodo,
+        id: id++,
+        completed: false
+      });
+      this.newTodo = "";
     },
     doneEdit(todo) {
       this.todos = this.todos.map(x => {
         return todo.id === x.id ? { ...todo } : { ...x };
       });
-      this.editTodo = {};
+      cancelTodo();
     },
-    cancelEdit() {
-      this.editTodo = {};
+    cancelTodo() {
+      this.editedTodo = {};
+    },
+    editTodo(todo) {
+      this.editedTodo = { ...todo };
     },
     removeTodo(todo) {
-      const index = this.todos.findIndex(x => x.id === todo.id);
-      this.todos.splice(index, 1);
-    },
-    addTodo() {
-      if (!this.newTodo) {
-        return;
-      }
-      this.todos.unshift({
-        id: id++,
-        title: this.newTodo,
-        completed: false
-      });
-      this.newTodo = "";
-    },
-    removeCompleted() {
-      this.todos = this.todos.filter(x => !x.completed);
+      const removeIndex = this.todos.findIndex(x => x.id === todo.id);
+      this.todos.splice(removeIndex, 1);
     }
   }
 };
@@ -108,9 +73,4 @@ export default {
 
 <style>
 @import "https://unpkg.com/todomvc-app-css@2.1.0/index.css";
-*,
-::after,
-::before {
-  box-sizing: inherit;
-}
 </style>
