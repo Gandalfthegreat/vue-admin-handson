924b4c140ec89faba56d96bd0ad7539757539d77
wangchao11 <wangchao11@kuaishou.com>
2021-01-24 20:13:35 +0800

    feat: todo comp

---
+++ src/views/todos/components/todo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="{editing: isEdited}">
+    <div class="view">
+      <input type="checkbox" class="toggle" @change="updateChecked($event.target.checked)" />
+      <label @dblclick="editTodo(todo)">{{title }}</label>
+      <button class="destory" @click="removeTodo(todo)"></button>
+    </div>
+    <input type="text" class="edit" v-model="editingTitle" v-autofoucs v-if="isEdited" @blur="doneEdit()" @keyup.enter="doneEdit()" @keyup.esc="cancelEdit()">
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
