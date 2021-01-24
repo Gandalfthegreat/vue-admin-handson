f3caaf1f91241b026516a7c9737f3545375e7a06
Gandalfthegreat <785918558@qq.com>
2021-01-24 21:13:00 +0800

    feat: todo comp

--- src/views/todos/components/todo.vue
+++ src/views/todos/components/todo.vue
@@ -5,12 +5,59 @@
       <label @dblclick="editTodo(todo)">{{title }}</label>
       <button class="destory" @click="removeTodo(todo)"></button>
     </div>
-    <input type="text" class="edit" v-model="editingTitle" v-autofoucs v-if="isEdited" @blur="doneEdit()" @keyup.enter="doneEdit()" @keyup.esc="cancelEdit()">
+    <input
+      type="text"
+      class="edit"
+      v-model="editingTitle"
+      v-autofoucs
+      v-if="isEdited"
+      @blur="doneEdit()"
+      @keyup.enter="doneEdit()"
+      @keyup.esc="cancelEdit()"
+    />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isEdited: false,
+      editingTitle: "",
+    };
+  },
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    editTodo() {
+      this.editingTitle = this.title;
+      this.isEdited = true;
+    },
+    doneEdit() {
+      if (this.isEdited) {
+        this.$emit("update:title", this.editingTitle);
+        this.isEdited = false;
+      }
+    },
+    cancelEdit() {
+      this.isEdited = false;
+    },
+    updateChecked(completed) {
+      this.$emit("update:completed", completed);
+    },
+    removeTodo() {
+      this.$emit("delete");
+    },
+  },
+};
 </script>
 
 <style>
